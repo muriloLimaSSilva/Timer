08ceb15073f00c77c587a2525bd1d5c4b7869c70
Murilo Lima <murilolimasouzaesilva@gmail.com>
2025-01-22 10:23:21 -0300

    Arquivos editados

--- src/main/java/com/example/timer/Timer.java
+++ src/main/java/com/example/timer/Timer.java
@@ -4,27 +4,22 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Timer {
-    String formatedTime;
-    int seconds;
-    int minutes;
-    int hours;
+    private String formatedTime;
+    private int seconds;
+    private int minutes;
+    private int hours;
 
-    public String formatTime(String formatedTime, int seconds, int minutes, int hours){
+
+    public String formatTime(int seconds, int minutes, int hours){
         this.seconds = seconds;
         this.minutes = minutes;
         this.hours = hours;
-        this.formatedTime = formatedTime;
 
         this.formatedTime = String.format("%02d:%02d:%02d", this.hours, this.minutes, this.seconds);
 
         return this.formatedTime;
     }
 
-    public void InitializePause(){
-        System.out.println("Iniciou");
-    }
 
-    public void Stop(){
-        System.out.println("parou");
-    }
+
 }
--- src/main/java/com/example/timer/TimerApplication.java
+++ src/main/java/com/example/timer/TimerApplication.java
@@ -1,22 +1,42 @@
 package com.example.timer;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.fxml.FXML;
-import javafx.event.ActionEvent;
-import javafx.scene.input.MouseEvent;
+
 import java.io.IOException;
 
 public class TimerApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Carregando o arquivo FXML
         FXMLLoader fxmlLoader = new FXMLLoader(TimerApplication.class.getResource("timer.fxml"));
+
+        // Criando nova cena com o FXML
         Scene scene = new Scene(fxmlLoader.load());
+
+        // Título da janela
         stage.setTitle("Timer");
         stage.setScene(scene);
+
+        // Exibindo a janela
         stage.show();
+    }
+
+    // Método para Iniciar/Pausar o cronômetro
+    @FXML
+    public void onStartPauseButtonClicked() {
+        System.out.println("Iniciou ou Pausou");
+
+    }
+
+    // Método para Parar o cronômetro
+    @FXML
+    public void onStopButtonClicked() {
+        System.out.println("Parou");
+
     }
 
     public static void main(String[] args) {

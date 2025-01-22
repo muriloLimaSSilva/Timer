250c981557084402b87c23be5bf1aaaf9e30bc20
Murilo Lima <murilolimasouzaesilva@gmail.com>
2025-01-22 14:25:24 -0300

    Atualização na funcionalidade do botao start, adição de labelTime em Timer.java, metodo de icrementação detempo parcialmenteconcluido e fortação de detemplo concluido

--- src/main/java/com/example/timer/Timer.java
+++ src/main/java/com/example/timer/Timer.java
@@ -10,22 +10,45 @@ public class Timer {
     private int minutes;
     private int hours;
 
+    @FXML
+    protected Label labelTime;
 
     public String formatTime(int seconds, int minutes, int hours){
         this.seconds = seconds;
         this.minutes = minutes;
         this.hours = hours;
 
+        // Atualiza a variável formatada
         this.formatedTime = String.format("%02d:%02d:%02d", this.hours, this.minutes, this.seconds);
 
         return this.formatedTime;
     }
 
-    public void onStartPause(ActionEvent event){
-        System.out.println("Oi");
+    public void IncrementTime(){
+        // Incrementa o tempo
+        this.seconds++;
+        if (this.seconds >= 60){
+            this.seconds = 0;
+            this.minutes++;
+            if (this.minutes >= 60){
+                this.minutes = 0;
+                this.hours++;
+            }
+        }
+
+        // Formata o tempo após o incremento
+        String currentTime = formatTime(this.seconds, this.minutes, this.hours);
+
+        System.out.println(currentTime); // Para ver o tempo no console
+        labelTime.setText(currentTime);
     }
 
+    public void onStartPause(ActionEvent event){
+        // Inicia o incremento do tempo
+        IncrementTime();
+    }
 
     public void onStop(ActionEvent event) {
+
     }
 }
